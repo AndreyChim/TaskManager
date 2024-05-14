35b442b65165cf34f1fa379ab5ed86fcee77880b
AndreyChim <chimitcyrenov@mail.ru>
2024-05-14 08:32:04 +0800

    Fix by rubocop

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -6,10 +6,9 @@ class Admin::UsersControllerTest < ActionController::TestCase
     sign_in admin
   end
 
-  test "should get show" do
+  test 'should get show' do
     user = create(:user)
     get :show, params: { id: user.id }
     assert_response :success
   end
-
 end
