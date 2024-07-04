6ca8663b69dfe63590af902191a8fe7120be6861
AndreyChim <chimitcyrenov@mail.ru>
2024-07-04 14:48:41 +0800

    Fix name of import

--- app/javascript/components/Task/Task.js
+++ app/javascript/components/Task/Task.js
@@ -5,7 +5,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import * as useStyles from './useStyles';
+import useStyles from './useStyles';
 
 function Task({ task }) {
   const styles = useStyles();
