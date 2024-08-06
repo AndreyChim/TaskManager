5f6ccc8b3ecca5728c97178597325f2ada0bbd22
AndreyChim <chimitcyrenov@mail.ru>
2024-08-06 21:34:17 +0800

    Add const action

--- app/javascript/components/Task/Task.js
+++ app/javascript/components/Task/Task.js
@@ -22,7 +22,7 @@ function Task({ task, onClick }) {
   return (
     <Card className={styles.root}>
       <CardHeader action={action} title={task.name} />
-      <CardHeader title={task.name} />
+      {/* <CardHeader title={task.name} /> */}
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {task.description}
