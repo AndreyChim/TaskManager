aeed971e3ab399397fa55a2315aa85f1b8c4ccd9
AndreyChim <chimitcyrenov@mail.ru>
2025-01-07 20:42:09 +0800

    Add mailer test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,8 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
   include AuthHelper
+  include ActionMailer::TestHelper
+  
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
