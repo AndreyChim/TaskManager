dfd30b798ec057bc375913f40c84803cbc5c31d0
AndreyChim <chimitcyrenov@mail.ru>
2025-01-08 17:47:09 +0800

    Fix test to send of email

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -19,13 +19,18 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     assignee = create(:user)
     task_attributes = attributes_for(:task).
       merge({ assignee_id: assignee.id })
-    post :create, params: { task: task_attributes, format: :json }
-    assert_response :created
+    
+    assert_emails 1 do
+      post :create, params: { task: task_attributes, format: :json }
+    end
+    assert_response :created  
 
     data = JSON.parse(response.body)
     created_task = Task.find(data['task']['id'])
 
     assert created_task.present?
+    assert created_task.assignee == assignee
+    assert created_task.author == author
     assert_equal task_attributes.stringify_keys, created_task.slice(*task_attributes.keys)
   end
 
