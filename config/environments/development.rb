45bd1d99b4def42303daee790f40492a9fcf90d5
AndreyChim <chimitcyrenov@mail.ru>
2025-06-02 17:20:55 +0800

    Add preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "taskmanager-tbkj.onrender.com"
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_caching = true
 end
