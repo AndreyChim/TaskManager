f3f36dbd75e95c82bd8381710e0773cf43552063
AndreyChim <chimitcyrenov@mail.ru>
2025-06-03 14:51:46 +0800

    Generate password reset migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_17_140558) do
+ActiveRecord::Schema.define(version: 2025_06_03_064623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "password_resets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "token"
+    t.datetime "expires_at"
+    t.boolean "used"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["token"], name: "index_password_resets_on_token", unique: true
+    t.index ["user_id"], name: "index_password_resets_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -37,4 +48,5 @@ ActiveRecord::Schema.define(version: 2024_04_17_140558) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "password_resets", "users"
 end
