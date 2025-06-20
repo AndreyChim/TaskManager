d7d7b7bddfa8644c1fae649a52d8efed4602e822
AndreyChim <chimitcyrenov@mail.ru>
2025-06-20 14:38:52 +0800

    Rename variable reset & Reset password for any user type

--- app/forms/password_reset_form.rb
+++ app/forms/password_reset_form.rb
@@ -1,16 +1,17 @@
 class PasswordResetForm
     include ActiveModel::Model
   
-    attr_accessor :token, :password, :password_confirmation, :reset
+    attr_accessor :token, :password, :password_confirmation
+    attr_reader :password_reset
   
     validates :password, presence: true, length: { minimum: 6 }
     validates :password_confirmation, presence: true
     validate :passwords_match
-    validate :reset_valid
+    validate :password_reset_valid
 
     def initialize(token:)
       @token = token
-      @reset = Admin.find_by(reset_token: token)
+      @password_reset = PasswordReset.find_by(token: token)
     end
   
     def passwords_match
@@ -18,20 +19,18 @@ class PasswordResetForm
       errors.add(:password_confirmation, "doesn't match Password")
     end
   
-    def reset_valid
-      self.reset = PasswordReset.find_by(token: token)
-      
-      if reset.blank?
+    def password_reset_valid
+      if password_reset.blank?
         errors.add(:base, 'Invalid token')
-      elsif !reset.still_valid?
+      elsif !password_reset.still_valid?
         errors.add(:base, 'Token has expired')
       end
     end
   
     def save
       return false unless valid?
-      reset.user.update!(password: password)
-      reset.mark_used!
+      password_reset.user.update!(password: password)
+      password_reset.mark_used!
       true
     end
 end
