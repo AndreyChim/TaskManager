43536dd363a2e9badaeb19a63fe9b9f2b71f9717
AndreyChim <chimitcyrenov@mail.ru>
2025-06-23 16:45:27 +0800

    Add tests for PasswordReset

--- test/models/password_reset_test.rb
+++ test/models/password_reset_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class PasswordResetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @reset = PasswordReset.create(user: @user)
+  end
+
+  test "requires user association" do
+    reset = PasswordReset.new(user: nil)
+    assert_not reset.valid?
+    assert_includes reset.errors[:user], "must exist"
+  end
+
+  test "generates unique token on creation" do
+    assert_not_nil @reset.token
+    duplicate = PasswordReset.new(user: @user, token: @reset.token)
+    assert_not duplicate.valid?
+  end
+
+  test "expiration status" do
+    assert_not @reset.expired?
+    @reset.update!(created_at: 2.hours.ago)
+    assert @reset.expired?
+  end
+
+  test "find_valid method" do
+    found = PasswordReset.find_valid(@reset.token)
+    assert_equal @reset, found
+    
+    @reset.update!(created_at: 2.hours.ago)
+    assert_nil PasswordReset.find_valid(@reset.token)
+    
+    assert_nil PasswordReset.find_valid("invalid_token")
+    
+    fresh_reset = PasswordReset.create(user: @user)
+    fresh_reset.consume!
+    assert_nil PasswordReset.find_valid(fresh_reset.token)
+  end
+
+  test "consumption" do
+    assert_not @reset.consumed?
+    @reset.consume!
+    assert @reset.consumed?
+  end
 end
