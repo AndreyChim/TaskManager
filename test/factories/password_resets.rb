0633024f29f96215d4738a91ddc0f7ac87709c64
AndreyChim <chimitcyrenov@mail.ru>
2025-06-23 14:58:31 +0800

    Use sequence

--- test/factories/password_resets.rb
+++ test/factories/password_resets.rb
@@ -1,8 +1,20 @@
 FactoryBot.define do
   factory :password_reset do
-    user { nil }
-    token { "MyString" }
-    expires_at { "2025-06-03 06:46:23" }
+    user
+    sequence(:token) { |n| "valid_token_#{n}#{SecureRandom.hex(10)}" }
+    expires_at { 1.hour.from_now }
     used { false }
+
+    trait :expired do
+      expires_at { 1.hour.ago }
+    end
+
+    trait :used do
+      used { true }
+    end
+
+    trait :with_known_token do
+      token { "known_test_token" }
+    end
   end
 end
