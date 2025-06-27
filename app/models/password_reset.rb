fc14c99471a9f5cc872edee1f5f7561e8b2d754e
AndreyChim <chimitcyrenov@mail.ru>
2025-06-27 14:01:20 +0800

    Add initialize_state

--- app/models/password_reset.rb
+++ app/models/password_reset.rb
@@ -2,12 +2,14 @@ class PasswordReset < ApplicationRecord
   belongs_to :user
 
   before_create :generate_token
-  validates :token, uniqueness: true
-
+  before_create :set_expiration_time
   after_create :trigger_password_reset_email
 
-  # State machine definition
+  validates :token, uniqueness: true
+
   state_machine :state, initial: :pending do
+    before_save :initialize_state, if: :new_record?
+
     event :mark_used do
       transition pending: :used
     end
@@ -19,20 +21,27 @@ class PasswordReset < ApplicationRecord
   end
 
   def still_valid?
-    pending? && expires_at > Time.current
+    pending? && expires_at && expires_at > Time.current
   end
 
   private
 
+  def initialize_state
+    self.state ||= 'pending'
+  end
+  
   def generate_token
     self.token = SecureRandom.urlsafe_base64(20)
+  end
+
+  def set_expiration_time
+    self.expires_at ||= 24.hours.from_now
   end
 
   def trigger_password_reset_email
     PasswordResetEmailSender.call(self)
   end
 
-   # Callback to update timestamp when marked used
   def update_used_timestamp
     update(used_at: Time.current)
   end
