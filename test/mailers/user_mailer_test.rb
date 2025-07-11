34c4176d4a12ebfc2bbdf5d098222a03ee8f5ed4
AndreyChim <chimitcyrenov@mail.ru>
2025-07-11 16:12:29 +0800

    Fix test email

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -32,6 +32,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal [NOREPLY_EMAIL], email.from
     assert_equal [user.email], email.to
     assert_equal 'Task Updated', email.subject
-    assert email.body.to_s.include?("Task #{task.id} was updated")
+    assert email.body.to_s.include?('Your task "')
+    assert email.body.to_s.include?('" has been modified')
   end
 end
